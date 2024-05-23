9d37ba7fe6c536c7ffee9f55aa8178fa9d517811
Costa05br <j.costa009@edu.pucrs.br>
2024-05-23 10:35:50 -0300

    Arrumando Arquivos

---
+++ BootCamp/src/Joao Lucas/Tópicos/Operadores.java
@@ -0,0 +1,14 @@
+public class Operadores {
+    public static void main(String[] args) {
+        System.out.println ("Anotações:");
+      
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+        System.out.println("Concatenação:" + " " + nomeCompleto);
+        
+        String concatenacao = "?";
+        concatenacao = 1+1+1+"1";
+
+        System.out.println (concatenacao);
+    }   
+
+}
---
+++ BootCamp/src/Joao Lucas/Tópicos/TiposVariaveis.java
@@ -0,0 +1,30 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Anotações:");
+
+        //Tipos primitivos:
+        
+        //byte idade = 123;
+        //short ano = 2021;
+        //int cep = 21070333;
+        //long cpf = 98765432109L;
+        //float pi = 3.14F;
+        //double salario = 1275.33; 
+        //boolean tabelaVerdade = true;
+        //char unicoCaracter = 'M';
+
+    
+        //Constantes:
+        // - Constantes são valores armazenados em uma variável com a palavra reservada "final" seguida do tipo da variável, nome e atribuição;
+        // - Nome da variável deve estar em CAIXA_ALTA;
+
+        // Por Exemplo:
+
+        //final double SALARIO_MINIMO = 1400.35;
+        
+        //Obs: Aqui, mesmo que atribuimos outro valor a variável "SALARIO_MINIMO", não irá mudar, pois o valor da constante não muda, continua o calor que você atribuiu no começo;
+        //Portanto, a própria IDE nos mostra que a um erro, pois não vai funcionar atribuir outro valor a constante;
+        
+         
+    }
+}
